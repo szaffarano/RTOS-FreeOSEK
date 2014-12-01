ae8668ef10af0b45e379c206104ed73ac7a78ffd
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2014-11-30 22:35:01 -0300

    Elimina código para simplificar

--- ejemplo/src/osek_blinky.c
+++ ejemplo/src/osek_blinky.c
@@ -8,31 +8,15 @@
  ===============================================================================
  */
 
-#if defined (__USE_LPCOPEN)
-#if defined(NO_BOARD_LIB)
-#include "chip.h"
-#else
 #include "board.h"
-#endif
-#endif
 
 #include <stdio.h>
 #include <cr_section_macros.h>
 
 #include "os.h"               /* <= operating system header */
 
-/*
- * See the OIL configuration file in
- * etc/config.oil
- */
-
-/* Comment this line in order to use LPC-Link based printf()
- * If uncommented, all printf() calls will be disabled
- */
 //#define printf(...)
 int main(void) {
-#if defined (__USE_LPCOPEN)
-#if !defined(NO_BOARD_LIB)
 	// Read clock settings and update SystemCoreClock variable
 	SystemCoreClockUpdate();
 	// Set up and initialize all required blocks and
@@ -43,9 +27,6 @@ int main(void) {
 	Chip_GPIO_SetPinDIR(LPC_GPIO, 0, 26, 1);
 	Chip_GPIO_SetPinDIR(LPC_GPIO, 2, 0, 1);
 	Chip_GPIO_SetPinDIR(LPC_GPIO, 2, 1, 1);
-
-#endif
-#endif
 
 	StartOS(AppMode1);
 
@@ -62,9 +43,6 @@ void ErrorHook(void) {
 }
 
 TASK(TaskInit) {
-	/* Set 500 tick alarm for TaskPeriodic */
-	SetRelAlarm(ActivateTaskBlink, 0, 500);
-
 	SetRelAlarm(ActivateTaskGreen, 500, 0);
 
 	/* end InitTask */
@@ -82,7 +60,7 @@ TASK(TaskGreen) {
 	Chip_GPIO_SetPinState(LPC_GPIO, 2, 1, 1);
 	Chip_GPIO_SetPinState(LPC_GPIO, 2, 0, 0);
 	Chip_GPIO_SetPinState(LPC_GPIO, 0, 26, 0);
-	SetRelAlarm(ActivateTaskBlue, 500, 0);
+	SetRelAlarm(ActivateTaskBlue, 100, 0);
 	TerminateTask();
 }
 TASK(TaskBlue) {
@@ -90,7 +68,7 @@ TASK(TaskBlue) {
 	Chip_GPIO_SetPinState(LPC_GPIO, 2, 1, 0);
 	Chip_GPIO_SetPinState(LPC_GPIO, 2, 0, 0);
 	Chip_GPIO_SetPinState(LPC_GPIO, 0, 26, 1);
-	SetRelAlarm(ActivateTaskRed, 500, 0);
+	SetRelAlarm(ActivateTaskRed, 100, 0);
 	TerminateTask();
 }
 TASK(TaskRed) {
@@ -98,6 +76,6 @@ TASK(TaskRed) {
 	Chip_GPIO_SetPinState(LPC_GPIO, 2, 1, 0);
 	Chip_GPIO_SetPinState(LPC_GPIO, 2, 0, 1);
 	Chip_GPIO_SetPinState(LPC_GPIO, 0, 26, 0);
-	SetRelAlarm(ActivateTaskGreen, 500, 0);
+	SetRelAlarm(ActivateTaskGreen, 100, 0);
 	TerminateTask();
 }
